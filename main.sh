e4564144fdb34a49e74ca6d896ae140cbb542469
Derek <derek@defiant>
2014-05-21 21:59:02 -0500

    holy crap none of this is working

--- main.sh
+++ main.sh
@@ -2,9 +2,8 @@
 #! /bin/bash
 
 while inotifywait -qq /home/derek/irc/chat.freenode.org/#pumpingstationone/out;
-do gawk ' 
-   COMMANDS="!awk";
-    if ($2 == "!awk") 
-      echo "Awk!" > /home/derek/irc/chat.freenode.org/#pumpingstationone/in;
-  '
+do
+LAST_LINE = $(tail -n 1 /home/derek/irc/chat.freenode.org/#pumpingstationone/out)
+gawk '{if($1=="!awk")
+        echo "Awk!" > /home/derek/irc/chat.freenode.org/#pumpingstationone/out}'
 done
