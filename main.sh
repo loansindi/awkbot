d496b0bda25734ece1d11275a61bbf9895f4b3ab
Derek <derek@defiant>
2014-06-04 21:45:11 -0500

    Remove old stuff, this is dev

--- main.sh
+++ main.sh
@@ -4,6 +4,4 @@
 while inotifywait -qq /home/derek/irc/chat.freenode.org/#pumpingstationone/out;
 do
 tail -n 1 "/home/derek/irc/chat.freenode.org/#pumpingstationone/out" | gawk '{if ($4 == "!awk") print "ward!" > "/home/derek/irc/chat.freenode.org/#pumpingstationone/in"}'
-# gawk -v LINE=$LAST_LINE LINE '{ if ($1)
- #       print "Awk!"}' 
 done
