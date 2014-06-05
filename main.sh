3eb63e4cdb2e3de918ef38e2263e237c9affa391
Derek <derek@defiant>
2014-06-05 10:33:47 -0500

    git ignore

--- main.sh
+++ main.sh
@@ -7,3 +7,5 @@ tail -n 1 "/home/derek/irc/chat.freenode.org/#pumpingstationone/out" > input
 gawk '{if ($4 == "!awk") print "ward!" > "/home/derek/irc/chat.freenode.org/#pumpingstationone/in"}' input
 gawk '{if ($4 == "!hawk") print "http://global3.memecdn.com/hawkward_o_361560.jpg" > "/home/derek/irc/chat.freenode.org/#pumpingstationone/in"}' input
 done
+
+
