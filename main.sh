25ef5a4c1a0be3ad3a36285818b16009c09efeba
Derek <derek.bever@gmail.com>
2014-06-05 16:55:46 +0000

    uh

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 # awkbot
 #! /bin/bash
 
-while inotifywait -qq /home/derek/irc/chat.freenode.org/#pumpingstationone/out;
+while inotifywait -qq ../#pumpingstationone/out;
 do
-tail -n 1 "/home/derek/irc/chat.freenode.org/#pumpingstationone/out" | gawk '{if ($4 == "!awk") print "ward!" > "/home/derek/irc/chat.freenode.org/#pumpingstationone/in"}'
+tail -n 1 "../#pumpingstationone/out" | gawk '{if ($4 == "!awk") print "ward!" > "../#pumpingstationone/in"}'
 done
