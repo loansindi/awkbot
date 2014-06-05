fcb4fad2381f971f54eaf59042da5ea0aab58dd0
Derek <derek@defiant>
2014-06-04 22:20:22 -0500

    Awesome jpg

--- main.sh
+++ main.sh
@@ -3,5 +3,7 @@
 
 while inotifywait -qq /home/derek/irc/chat.freenode.org/#pumpingstationone/out;
 do
-tail -n 1 "/home/derek/irc/chat.freenode.org/#pumpingstationone/out" | gawk '{if ($4 == "!awk") print "ward!" > "/home/derek/irc/chat.freenode.org/#pumpingstationone/in"}'
+tail -n 1 "/home/derek/irc/chat.freenode.org/#pumpingstationone/out" > input
+gawk '{if ($4 == "!awk") print "ward!" > "/home/derek/irc/chat.freenode.org/#pumpingstationone/in"}' input
+gawk '{if ($4 == "!hawk") print "http://global3.memecdn.com/hawkward_o_361560.jpg" > "/home/derek/irc/chat.freenode.org/#pumpingstationone/in"}' input
 done
