a96eb03dbbd1e5151ff9926ad9b4627098c7ae91
Derek <derek.bever@gmail.com>
2014-06-05 18:07:36 +0000

    actually remove channel name requirement

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
 # awkbot
 #! /bin/bash
 # Place the 'awkbot' folder in the ii channel directory that you intend to run awkbot in.
-while inotifywait -qq ../$CHAN/out;
+while inotifywait -qq ../out;
 do
-tail -n 1 "../$CHAN/out" > input
+tail -n 1 "../out" > input
 gawk  '{if ($4 == "!awk") print "ward!" > "../in"}' input
 gawk  '{if ($4 == "!hawk") print "http://global3.memecdn.com/hawkward_o_361560.jpg" > "../in"}' input
 done
